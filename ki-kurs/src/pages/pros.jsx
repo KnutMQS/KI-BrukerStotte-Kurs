227de8003a2deda6aad8d637881c422a6bd6c9ec
KnutMQS <knutmqs@gmail.com>
2025-11-23 14:25:22 +0100

    feat: update Pros component to display benefits of AI

--- ki-kurs/src/pages/pros.jsx
+++ ki-kurs/src/pages/pros.jsx
@@ -1,11 +1,48 @@
 function Pros() {
   return (
-    <div className="flex items-center justify-center p-4 text-9xl font-extrabold">
-      <span>
-        Pros incoming... <br />
-        <span className="flex items-center justify-center"> maybe </span>
-      </span>
-    </div>
+    <>
+      <div className="flex items-center justify-center text-3xl py-4">
+        <h1>Fordeler med KI</h1>
+      </div>
+      <div className="space-y-7">
+        <ul className="max-w-fit mx-auto border rounded-4xl border-indigo-600 p-4">
+          <li className="space-y-3">
+            <h3 className="flex justify-center items-center text-2xl underline">
+              Automasjon og effekivitet:
+            </h3>
+            <span>
+              KI kan automatisere repetitive oppgaver og øke produktiviteten
+            </span>
+          </li>
+        </ul>
+        <ul className="max-w-fit mx-auto border rounded-4xl border-indigo-600 p-4">
+          <li className="space-y-3">
+            <h3 className="flex justify-center items-center text-2xl underline">
+              Redusert Menneskelige Feil:
+            </h3>
+            <span>
+              Konsistente og nøyaktige resultater i definerte oppgaver
+            </span>
+          </li>
+        </ul>
+        <ul className="max-w-fit mx-auto border rounded-4xl border-indigo-600 p-4">
+          <li className="space-y-3">
+            <h3 className="flex justify-center items-center text-2xl underline">
+              Tilgjengelig 24/7:
+            </h3>
+            <span>KI systemer kan jobber kontinuerlig uten pauser</span>
+          </li>
+        </ul>
+        <ul className="max-w-fit mx-auto border rounded-4xl border-indigo-600 p-4">
+          <li className="space-y-3">
+            <h3 className="flex justify-center items-center text-2xl underline">
+              Data Analyse:
+            </h3>
+            <span>Behandle og analysere store datasett raskt</span>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
