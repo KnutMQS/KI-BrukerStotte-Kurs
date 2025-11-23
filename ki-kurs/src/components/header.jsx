e3a0d7cb50b787b96b455b3d107ec3943d49d14b
KnutMQS <knutmqs@gmail.com>
2025-11-23 13:46:07 +0100

    feat: add Header component with navigation links

---
+++ ki-kurs/src/components/header.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <div className="flex justify-center items-center text-5xl bg-linear-to-r from-indigo-500 to-indigo-950 border-b-2 border-border pb-4 pt-2 font-mono font-extrabold text-shadow-lg/60">
+        <h1>KI Oppgave</h1>
+      </div>
+      <nav className="flex justify-around mx-auto max-w-3xl items-center font-mono border-2 border-t-0 border-border py-2 bg-button-bg">
+        <Link className="hover:text-blue-400" to="/">
+          Home
+        </Link>
+        <Link className="hover:text-blue-400" to="/pros">
+          Pros med KI
+        </Link>
+        <Link className="hover:text-blue-400" to="/cons">
+          Cons med KI
+        </Link>
+        <Link className="hover:text-blue-400" to="/society">
+          AI i Samfunnet
+        </Link>
+        <Link className="hover:text-blue-400" to="/it-industry">
+          AI i IT-Bransjen
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
