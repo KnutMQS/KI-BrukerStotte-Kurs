be014dc8f68e24ff9a9a0b2201ea3e94754a9664
KnutMQS <knutmqs@gmail.com>
2025-11-25 08:36:38 +0100

    Slight styling change and link text update

--- ki-kurs/src/components/header.jsx
+++ ki-kurs/src/components/header.jsx
@@ -6,15 +6,15 @@ function Header() {
       <div className="flex justify-center items-center text-5xl bg-linear-to-r from-indigo-500 to-indigo-950 border-b-2 border-border pb-4 pt-2 font-mono font-extrabold text-shadow-lg/60">
         <h1>KI Oppgave</h1>
       </div>
-      <nav className="flex justify-around mx-auto max-w-3xl items-center font-mono border-2 border-t-0 border-border py-2 bg-button-bg">
+      <nav className="flex justify-around mx-auto max-w-3xl items-center font-mono border-2 border-t-0 rounded-b-xl border-border py-2 bg-button-bg">
         <Link className="hover:text-blue-400" to="/">
           Hjem
         </Link>
         <Link className="hover:text-blue-400" to="/pros">
-          Pros med KI
+          Fordeler
         </Link>
         <Link className="hover:text-blue-400" to="/cons">
-          Cons med KI
+          Ulemper
         </Link>
         <Link className="hover:text-blue-400" to="/society">
           KI i Samfunnet
